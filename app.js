ac89801ee28160f693f219acbc202b268bae4a8b
Vasily <gerasimovv1990@gmail.com>
2021-02-07 08:00:19 +0300

    v 0.1.4 - meta-title renaming

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 const { remote } = require('electron');
 const { dialog } = remote;
+const jsmediatags = require('jsmediatags');
+const ID3Writer = require('browser-id3-writer');
 const fs = require('fs');
-const { 
-    createListItemTemplate, 
+const {
+    createListItemTemplate,
     getFileNameFromPath,
-    splitFileNameForExtension,
 } = require('./utils');
 
-
 class App {
-
     selectFileBtn = document.querySelector('.js-select-file-btn');
     selectTypeRadios = document.querySelectorAll('.js-type');
     titleInput = document.querySelector('.js-title-input');
     postfixInput = document.querySelector('.js-postfix-input');
-    controlsForm = document.querySelector('.js-controls-form')
+    controlsForm = document.querySelector('.js-controls-form');
     runBtn = document.querySelector('.js-run-btn');
     itemsList = document.querySelector('.js-items-list');
     errorMsgEl = document.querySelector('.js-error-msg');
@@ -26,6 +25,7 @@ class App {
     selectedType = null;
     titleInputValue = '';
     postfixInputValue = 'AnimeNewMusic';
+    doneItemsCounter = 0;
 
     constructor() {
         this.init();
@@ -36,31 +36,43 @@ class App {
     }
 
     addListeners() {
-        this.selectFileBtn.addEventListener('click', this.onFilesSelect.bind(this), false);
-        this.titleInput.addEventListener('keyup', this.onTitleInputChange, false);
-        this.selectTypeRadios.forEach(radio => {
+        this.selectFileBtn.addEventListener(
+            'click',
+            this.onFilesSelect.bind(this),
+            false
+        );
+        this.titleInput.addEventListener(
+            'keyup',
+            this.onTitleInputChange,
+            false
+        );
+        this.selectTypeRadios.forEach((radio) => {
             radio.addEventListener('change', this.onTypeChange, false);
         });
-        this.controlsForm.addEventListener('submit', this.onBeforeProccess, false);
+        this.controlsForm.addEventListener(
+            'submit',
+            this.onBeforeStart,
+            false
+        );
     }
 
     getSelectedType() {
         let selectedType;
-        this.selectTypeRadios.forEach(radio => {
+        this.selectTypeRadios.forEach((radio) => {
             if (radio.checked) {
                 selectedType = radio.value;
             }
-        })
+        });
         return selectedType;
     }
 
     onTitleInputChange = (e) => {
         this.titleInputValue = e.target.value;
-    }
+    };
 
     onTypeChange = (e) => {
         this.selectedType = e.target.value;
-    }
+    };
 
     toggleErrorMsg(show = true) {
         this.errorMsgEl.style.display = show ? 'block' : 'none';
@@ -68,13 +80,20 @@ class App {
 
     toggleSuccessMsg = (show = true) => {
         this.successMsg.style.display = show ? 'block' : 'none';
-    }
+    };
 
     onFilesSelect() {
         const t = this;
-        dialog.showOpenDialog({
-            properties: ['openFile', 'multiSelections']
-        })
+        dialog
+            .showOpenDialog({
+                properties: ['openFile', 'multiSelections'],
+                filters: [
+                    {
+                        name: 'MP3 Files',
+                        extensions: ['mp3'],
+                    },
+                ],
+            })
             .then(function (dialogProps) {
                 const { filePaths, canceled } = dialogProps;
                 if (canceled) {
@@ -84,7 +103,7 @@ class App {
                 if (filePaths === undefined) {
                     alert('No file selected');
                 } else {
-                    t.renderItemsList(filePaths)
+                    t.renderItemsList(filePaths);
                 }
             });
     }
@@ -92,14 +111,14 @@ class App {
     renderItemsList(filePaths) {
         const preparedItems = this.prepareItems(filePaths);
         if (preparedItems && preparedItems.length) {
-            preparedItems.forEach(item => {
+            preparedItems.forEach((item) => {
                 this.itemsList.appendChild(item);
             });
         }
     }
 
     prepareItems(filePaths) {
-        return filePaths.map(path => {
+        return filePaths.map((path) => {
             const fileName = getFileNameFromPath(path);
             const newItemEl = this.domParser.parseFromString(
                 createListItemTemplate({ fileName, path }),
@@ -111,11 +130,17 @@ class App {
     }
 
     addListItemListener(item) {
-        item.querySelector('input').addEventListener('change', this.toggleItemSelected);
+        item.querySelector('input').addEventListener(
+            'change',
+            this.toggleItemSelected
+        );
     }
 
     removeListItemListener(item) {
-        item.querySelector('input').removeEventListener('change', this.toggleItemSelected);
+        item.querySelector('input').removeEventListener(
+            'change',
+            this.toggleItemSelected
+        );
     }
 
     toggleItemSelected = (e) => {
@@ -127,11 +152,13 @@ class App {
                 this.selectedItems.push(item);
             }
         }
-    }
+    };
 
     clearListItemsListeners = () => {
-        [...this.itemsList.children].forEach(item => this.removeListItemListener(item));
-    }
+        [...this.itemsList.children].forEach((item) =>
+            this.removeListItemListener(item)
+        );
+    };
 
     clearSelectedItems() {
         this.selectedItems = [];
@@ -145,67 +172,112 @@ class App {
 
     resetTitleInputVal = () => {
         this.titleInput.value = this.titleInputValue = '';
-    }
+    };
 
     resetTypeRadios = () => {
         this.selectedType = null;
-        this.selectTypeRadios.forEach(radio => {
+        this.selectTypeRadios.forEach((radio) => {
             radio.checked = false;
         });
-    }
+    };
 
     resetAppData = () => {
+        this.doneItemsCounter = 0;
         this.resetTitleInputVal();
         this.resetTypeRadios();
         this.clearItemsList();
-    }
+    };
 
-    onBeforeProccess = (e) => {
+    onBeforeStart = (e) => {
         e.preventDefault();
-        if (!this.titleInputValue || !this.selectedItems.length 
-            || !this.selectedType) {
-                this.toggleErrorMsg();
-                setTimeout(function () {
+        if (
+            !this.titleInputValue ||
+            !this.selectedItems.length ||
+            !this.selectedType
+        ) {
+            this.toggleErrorMsg();
+            setTimeout(
+                function () {
                     this.toggleErrorMsg(false);
-                }.bind(this), 2000);
-                return;
+                }.bind(this),
+                2000
+            );
+            return;
         }
-        return this.runProccess();
-    } 
+        return this.runProcess();
+    };
 
-    runProccess = () => {
-        this.renameSelectedItems();
+    runProcess = async () => {
+        await this.renameSelectedItems();
+    };
+
+    finishProcess = () => {
         this.toggleSuccessMsg();
-        setTimeout(function() {
-            this.toggleSuccessMsg(false);
-        }.bind(this), 3500);
+        setTimeout(
+            function () {
+                this.toggleSuccessMsg(false);
+            }.bind(this),
+            3500
+        );
         this.resetAppData();
     }
 
     generatePrefix = () => `[${this.titleInputValue} ${this.selectedType}]`;
 
-    renameSelectedItems = () => {
+    renameSelectedItems = async () => {
         const t = this;
         const prefix = this.generatePrefix();
-        this.selectedItems.forEach((selectedItem) => {
-            t.renameItem(JSON.parse(selectedItem), prefix);
-        })
+        await this.selectedItems.forEach(async (selectedItem, index) => {
+            await t.renameItem(
+                JSON.parse(selectedItem), 
+                prefix,
+                index === t.selectedItems.length - 1
+            );
+        });
+    };
+
+    readTitleFromMeta = async (opts) => {
+        const { item, successCallback } = opts;
+        return await new jsmediatags.Reader(item.path)
+            .setTagsToRead(['TIT2', 'TT2'])
+            .read({
+                onSuccess: (tag) => {
+                    return successCallback(tag.tags, opts);
+                },
+                onError: function(err) {
+                    console.error(err);
+                }
+            });
     }
 
-    renameItem = (item, prefix) => {
-        const [fileName, fileExtension] = splitFileNameForExtension(item.fileName);
-        const newPath = item.path.replace(item.fileName, `${fileName} ${prefix}[${this.postfixInputValue}].${fileExtension}`);
-        fs.rename(item.path, newPath, function (err, data) {
-            if (err) {
-                alert('An error ocurred updating the file' + err.message);
-                console.error(err);
-                return;
+    processFile = (tags, opts) => {
+        const { item, prefix, isLast } = opts;
+        try {
+            const initialFileBuffer = fs.readFileSync(item.path);
+            const writer = new ID3Writer(initialFileBuffer);
+            writer.setFrame(
+                'TIT2',
+                `${tags.title} ${prefix}[${this.postfixInputValue}]`
+            );
+            writer.addTag();
+            const taggedSongBuffer = Buffer.from(writer.arrayBuffer);
+            fs.writeFileSync(item.path, taggedSongBuffer);
+            this.doneItemsCounter++;
+            if (isLast) {
+                alert('Success');
+                this.finishProcess();
+                window.location.reload();
             }
-            console.log(`renamed ${item.fileName} to ${prefix}${item.fileName}`);
-        })
+        } catch (e) {
+            console.error(e);
+        }
     }
 
+    renameItem = async (item, prefix, isLast) => {
+        await this.readTitleFromMeta({
+            item, prefix, isLast, successCallback: this.processFile,
+        });
+    };
 }
 
-
-new App();
+module.exports = App;
